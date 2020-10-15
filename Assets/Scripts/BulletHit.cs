c8894b7eadf15066dbeeb3d609b0f58f892389b7
pietschj <joachim.pietsch@iadt.ie>
2020-10-16 00:42:07 +0100

    FPS 2nd stage
    Build basic engine

---
+++ Assets/Scripts/BulletHit.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+public class BulletHit : MonoBehaviour
+{
+ //When we touch the collider we disable this object.
+ void OnCollisionEnter()
+ {
+    Debug.Log("disabled");
+    gameObject.SetActive(false);
+ }
+
+
+}

46688df24c71a42b4246933093e2cc3d74266ac1
freshfruit <pietschorama@gmail.com>
2022-10-07 11:46:09 +0100

    new scene with students

--- Assets/Scripts/ActivateProjectile.cs
+++ Assets/Scripts/ActivateProjectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class ActivateProjectile : MonoBehaviour
 {
     public GameObject projectile;
+    public float lifeTime = 2.0f;
     private GameObject clone;
 
     // Update is called once per frame
@@ -15,9 +16,8 @@ public class ActivateProjectile : MonoBehaviour
             clone = Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation);
 
             //destroy bullet after 2sec
-            Destroy(clone, 2.0f);
+            Destroy(clone, lifeTime);
         }
-
     }
 }
 

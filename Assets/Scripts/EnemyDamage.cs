b46866b1a6bcda49742f7122f3637c86d2338abf
Joachim Pietsch <pietschj@iadt.ie>
2022-10-13 16:07:15 +0100

    new scripts

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -1,29 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    //Private means only this script can access the variable.
     private int hitNumber;
 
-    // CollisionHandler. Other is a collision object it collides with
-    void OnCollisionEnter(Collision other)
-    {
-        	if(other.transform.CompareTag("bullet")) 
-            {
-                hitNumber++;
-            }
-
-            if (hitNumber == 3) 
-            {
-                Debug.Log("Zombie dead!");
-                Destroy(gameObject);
-            }
+    private void  OnEnable() {
+        hitNumber = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    //Unity stores the collider it hits and we can access it via the name other.
+    void OnCollisionEnter(Collision other)
+    {   
+        Debug.Log("Collision");
         
+        //We compare the tag in the other object to the tag name we set earlier.
+        if (other.transform.CompareTag("bullet"))
+        {
+            //If the comparison is true, we increase the hit number.
+            hitNumber++;
+            Debug.Log(hitNumber);
+        }
+        //if the hit number is equal to 3 we destroy this object.
+        if (hitNumber == 3)
+        {
+            gameObject.SetActive(false); 
+            //Destroy(gameObject);
+        }
     }
 }

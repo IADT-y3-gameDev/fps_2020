9bd2553670b0e62efe14d266b47395cd6a060163
Joachim Pietsch <pietschj@iadt.ie>
2022-10-20 19:07:39 +0100

    stage6 updated from last year

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -12,15 +12,14 @@ public class EnemyDamage : MonoBehaviour
     //Unity stores the collider it hits and we can access it via the name other.
     void OnCollisionEnter(Collision other)
     {   
-        Debug.Log("Collision");
+        //Debug.Log("Collision");
         
         //We compare the tag in the other object to the tag name we set earlier.
         if (other.transform.CompareTag("bullet"))
-       
         {
             //If the comparison is true, we increase the hit number.
             hitNumber++;
-             Debug.Log("with Bullet. Hit: " + hitNumber);
+            //Debug.Log(hitNumber);
         }
         //if the hit number is equal to 3 we destroy this object.
         if (hitNumber == 3)
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Spawners
+{
+    public GameObject go;
+    public bool active;
+    public Spawners(GameObject newGo, bool newBool)
+    {
+        go = newGo;
+        active = newBool;
+    }
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject panel;
+    public delegate void RestartRounds();
+    public static event RestartRounds RoundComplete;
+
+    private int health;
+    private int roundsSurvived;
+    private int currentRound;
+    private PlayerDamage playerDamage;
+    private Text panelText;
+
+    public List<Spawners> spawner = new List<Spawners>();
+
+    void Start()
+    {
+        Debug.Log("Game Manager");
+        Time.timeScale = 1;
+        panel.SetActive(false);
+        currentRound = 0;
+        playerDamage = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDamage>();
+        panelText = panel.GetComponentInChildren<Text>();
+        panelText.text = string.Format("Round {0}", currentRound+1);
+        panelText.text = "";
+        foreach (GameObject go in GameObject.FindObjectsOfType(typeof(GameObject)))
+        {
+            if (go.name.Contains("Spawner"))
+            {
+                spawner.Add(new Spawners(go, true));
+            }
+        }
+    }
+
+    void Update()
+    {   
+        
+        int total = 0;
+        health = playerDamage.health;
+        //player still alive
+        if (health > 0)
+        {
+            for (int i = spawner.Count - 1; i >= 0; i--)
+            {
+                if (spawner[i].go.GetComponent<Spawner>().spawnsDead)
+                {
+                    total++;
+                }
+            }
+
+            if (total == spawner.Count && roundsSurvived == currentRound)
+            {
+                roundsSurvived++;
+                Debug.Log(roundsSurvived);
+                panelText.text = string.Format("Round {0} Completed! Press Right mouse button for next wave!", roundsSurvived);
+                panel.SetActive(true);
+            }
+
+            //going to the next round
+            if (roundsSurvived != currentRound && Input.GetButton("Fire2"))
+            {
+                currentRound = roundsSurvived;
+                RoundComplete();
+                panelText.text = string.Format("Round {0}", currentRound+1);
+                panel.SetActive(false);
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                Scene current = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(current.name);
+            }
+            else
+            {   
+                //Game Over Message.
+                panel.SetActive(true);
+                panelText.text = string.Format("Game Over! Survived {0} Rounds", roundsSurvived);
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
